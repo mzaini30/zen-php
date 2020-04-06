d719d4d13847ef4814d4bb5dc233ac6ef4a42e99
mzaini30 <muhzaini30@gmail.com>
2020-04-06 15:28:25 +0800

    upload

--- index.php
+++ index.php
@@ -3,18 +3,31 @@
 include 'vendor/autoload.php';
 
 $link = $_SERVER["REQUEST_URI"];
-if ($link == "/"){
-  $link = "/index";
-}
-$target = "public" . $link;
 
-$link1 = $target . ".html";
-$link2 = $target . ".php";
+// jika tidak dimulai dari /api
 
-if(file_exists($link1)){
-  require_once($link1);
-} else if (file_exists($link2)){
-  require_once($link2);
+if (substr($link, 0, 5) !== "/api/"){
+
+  if ($link == "/"){
+    $link = "/index";
+  }
+  $target = "public" . $link;
+  
+  $link1 = $target . ".html";
+  $link2 = $target . ".php";
+  
+  if(file_exists($link1)){
+    require_once($link1);
+  } else if (file_exists($link2)){
+    require_once($link2);
+  } else {
+    require_once("script/404.html");
+  }
+
 } else {
-  require_once("script/404.html");
+  header("Content-Type: application/json");
+  $method = $_SERVER["REQUEST_METHOD"];
+  $data = $_POST;
+  $jsonServer = new JsonServer\JsonServer;
+  $jsonServer->handleRequest($method, $link, $data)->send();
 }
